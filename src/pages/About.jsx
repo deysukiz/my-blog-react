b89e39073474d9927622ccfd8b80b55e8eecfa73
deysukiz <deysukiz@gmail.com>
2021-08-04 00:23:50 -0500

    Front end of blog, react practice

---
+++ src/pages/About.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
+        About
+      </h1>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat
+        ratione reprehenderit officiis architecto ducimus fugit molestiae magnam
+        quo. Obcaecati rem labore aspernatur repellendus veniam dolore
+        repudiandae tenetur voluptate nobis voluptates unde iusto quasi
+        similique dolorem vero consequatur temporibus provident fugiat minima,
+        reprehenderit libero numquam eveniet aut autem! Rem, cupiditate
+        excepturi?
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Vel rem ea tenetur tempore eum veritatis fugiat laborum minus hic,
+        quaerat illo eos, ad ex, nemo iste suscipit ut illum voluptate! Dolore,
+        laborum consequuntur ex aspernatur deleniti ullam commodi quo facilis
+        unde in ipsum? Quo exercitationem animi officiis quisquam odio quod
+        ratione, saepe adipisci pariatur aspernatur atque provident! Commodi?
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae
+        perferendis earum qui repudiandae exercitationem iste quae esse
+        aspernatur libero voluptates obcaecati porro, consectetur optio. Tempora
+        maiores repellat tenetur hic atque omnis fugit non neque! Pariatur quia,
+        perspiciatis ipsam aliquid fugit suscipit quam ea consequatur debitis
+        exercitationem veritatis necessitatibus odit eaque inventore eius, culpa
+        molestias, officiis porro? Delectus, velit dolorem reiciendis
+        perspiciatis, totam quisquam, pariatur aspernatur repellendus cumque
+        dignissimos quod. Minima aliquam excepturi tenetur placeat quam facilis
+        nesciunt quis itaque fugiat enim ab impedit aliquid atque in
+        voluptatibus dolore ratione et ducimus aspernatur dolorum asperiores, ex
+        expedita. Fugiat animi iure porro?
+      </p>
+    </div>
+  );
+};
+
+export default About;
